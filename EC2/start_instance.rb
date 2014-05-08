91999a47f8c642d4a4cf8101b63662d708d4f69a
Vincent Daubry <vdaubry@gmail.com>
2014-05-09 00:52:21 +0200

    script scrappings

--- EC2/start_instance.rb
+++ EC2/start_instance.rb
@@ -12,7 +12,7 @@ group = ec2.security_groups['sg-e64cad83']
 
 puts "Request instance"
 request = ec2.instances.create(
-:image_id => 'ami-c15096b6',
+:image_id => 'ami-c767a1b0',
 :instance_type => 't1.micro',
 :count => 1,
 :security_groups => group, 
---
+++ OVH/scrap_tumblr1.sh
@@ -0,0 +1,1 @@
+ssh deploy@54.72.162.77 'cd /srv/www/photo-scrapper/current; nohup ruby scrappers/run_tumblr1_scrapping.rb >> log/scrapping.log 2>> log/scrapping.log < /dev/null &'
