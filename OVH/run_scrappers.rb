5662dd6dbb7acec65b5c5d8f907f5a10f8c77f74
Vincent Daubry <vdaubry@gmail.com>
2014-07-27 15:54:07 +0200

    script de scrapping de tous les ites

---
+++ OVH/run_scrappers.rb
@@ -0,0 +1,39 @@
+require 'aws-sdk'
+require 'yaml'
+
+conf = YAML.load_file("../config.yml")
+    
+AWS.config({
+  :access_key_id => conf["access_key_id"],
+  :secret_access_key => conf["secret_access_key"],
+  :region => "eu-west-1"
+})
+
+sqs = AWS::SQS.new 
+
+@queue = begin
+          sqs.queues.named(conf["website_queue_name"])
+        rescue AWS::SQS::Errors::NonExistentQueue => e
+          sqs.queues.create(conf["website_queue_name"],
+            :visibility_timeout => 3600,
+            :message_retention_period => 1209600)
+        end
+
+scrappers = []
+
+#Scrap forums
+(1..5).each { |i| scrappers << "forum#{i}" }
+
+#Scrap websites
+(1..2).each { |i| scrappers << "website#{i}" }
+
+#Scrap tumblrs
+(1..9).each { |i| scrappers << "tumblr#{i}" }
+
+scrappers.each do |key| 
+  json = {:website_key => key}.to_json
+  @queue.send_message("#{json}")
+end
+
+
+
